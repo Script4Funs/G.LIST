32c4daaea525dd73a211a0bcf7c762fab8e7cf26
Script4Fun <hensonflores0505@gmail.com>
2026-05-26 00:54:29 +0800

    Update com.Polywar.lua

--- com.Polywar.lua
+++ com.Polywar.lua
@@ -1,8 +1,3 @@
---[[
-Encrypted by Hackers House
-Tool : https://hackershouse.tech/online-lua-obfuscator-free-unlimited
-Version: 1.1.0
-]]
 _G["T_Y_nP4a3"]=function(src)
   local box={__src=src}
   return setmetatable({}, {
