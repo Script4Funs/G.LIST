063e02446d754b8f72e8e0a7160028098105c349
Script4Fun <hensonflores0505@gmail.com>
2026-06-01 23:56:12 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -22,7 +22,7 @@ local FREE_GAMES = {
     { name = "Soccer Battle", version = "Auto Update", script = "https://raw.githubusercontent.com/Script4Funs/G.LIST/refs/heads/main/com.Soccer%20Battle.lua" }, 
     { name = "Let's Go Rain", version = "Auto Update", script = "https://raw.githubusercontent.com/Script4Funs/G.LIST/refs/heads/main/com.Let's%20Go%20Rain.lua" }, 
     { name = "Mask Gun", version = "Auto Update", script = "https://raw.githubusercontent.com/Script4Funs/G.LIST/refs/heads/main/com.Mask%20Gun.lua" },
-    { name = "Coming Soon", version = "Auto Update", script = "https://raw.githubusercontent.com/USERNAME/REPO/main/codm.lua" },
+    { name = "Idle Hunter: Eternal Soul", version = "Auto Update", script = "https://raw.githubusercontent.com/Script4Funs/G.LIST/refs/heads/main/com.Idle%20Hunter_%20Eternal%20Soul.lua" },
     { name = "Coming Soon", version = "Auto Update", script = "https://raw.githubusercontent.com/USERNAME/REPO/main/codm.lua" },
     { name = "Coming Soon", version = "Auto Update", script = "https://raw.githubusercontent.com/USERNAME/REPO/main/codm.lua" },
     { name = "Coming Soon", version = "Auto Update", script = "https://raw.githubusercontent.com/USERNAME/REPO/main/codm.lua" },
